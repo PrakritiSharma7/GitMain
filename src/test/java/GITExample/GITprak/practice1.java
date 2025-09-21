fcbff5da52d84cd9e60618e8ed7f751eef4cf6a6
vansh <sharmaprakriti754@gmail.com>
2025-09-21 01:05:45 -0400

    first commit

---
+++ src/test/java/GITExample/GITprak/practice1.java
@@ -0,0 +1,14 @@
+package GITExample.GITprak;
+
+import org.junit.jupiter.api.Test;
+
+public class practice1 {
+	
+	@Test
+	public void code1() {
+		System.out.println("Let's practice");
+		
+		
+	}
+
+}
